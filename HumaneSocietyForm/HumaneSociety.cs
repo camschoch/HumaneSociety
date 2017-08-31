7467872353221e26f0257a956219377bfbb82244
camschoch <camschoch3@gmail.com>
2017-08-31 11:56:14 -0500

    made and added tables

--- HumaneSocietyForm/HumaneSociety.cs
+++ HumaneSocietyForm/HumaneSociety.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace HumaneSocietyForm
 {
-    public partial class Form1 : Form
+    public partial class HumaneSociety : Form
     {
-        public Form1()
+        public HumaneSociety()
         {
             InitializeComponent();
         }
