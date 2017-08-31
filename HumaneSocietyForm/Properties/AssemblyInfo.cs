ad54661a62b89e873d6f979e59ad5fe9dc48dd46
camschoch <camschoch3@gmail.com>
2017-08-31 09:29:42 -0500

    deleted console app

--- HumaneSocietyForm/Properties/AssemblyInfo.cs
+++ HumaneSocietyForm/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HumaneSociety")]
+[assembly: AssemblyTitle("HumaneSocietyForm")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HumaneSociety")]
+[assembly: AssemblyProduct("HumaneSocietyForm")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c185c311-9aad-4fc8-bac9-b9b375d20b78")]
+[assembly: Guid("dd2e7265-8c6b-4d33-b168-f15368db9bcb")]
 
 // Version information for an assembly consists of the following four values:
 //
